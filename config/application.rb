56efd25619828b090aad8c783cefaa9b883a42a5
豊岡　潤一郎 <>
2018-04-30 14:00:10 +0900

    Add gems for Heroku

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module PetsHuggy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +24,8 @@ module PetsHuggy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
